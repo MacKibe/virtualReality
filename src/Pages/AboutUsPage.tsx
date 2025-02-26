00979e73d97d216cab1d7b2dec70a3882f6c7d99
Mackibe <ndichujames2030@gmail.com>
2025-02-26 11:06:10 +0300

    Done with Service and About page usmobile view

--- src/Pages/AboutUsPage.tsx
+++ src/Pages/AboutUsPage.tsx
@@ -1,28 +1,28 @@
 const AboutUsPage = ({ coreValues }) => {
   return (
     <div className="py-8">
-      <div className="flex flex-col w-[75%] mx-auto justify-evenly h-[70dvh] gap-[5%]">
+      <div className="flex flex-col px-[15%] md:px-[25%] py-[5%] justify-evenly gap-[5%]">
         <div className="flex justify-between">
           <h3 className="flex items-center text-2xl">Mission</h3>
-          <p className="w-[70%] text-4xl text-[var(--primary)]">
+          <p className="w-[70%] text-[var(--primary)]">
             To redefine digital marketing by merging innovative VR technology
             with proven marketing strategies, delivering measurable results and
             creating memorable client experiences.
           </p>
         </div>
-        <hr className="text-[var(--primary)] border-2" />
+        <hr className="text-[var(--primary)] border-2 my-[5%]" />
         <div className="flex justify-between">
           <h3 className="flex items-center text-2xl">Vision</h3>
-          <p className="w-[70%] text-4xl text-[var(--primary)]">
+          <p className="w-[70%] text-[var(--primary)]">
             To become the premier digital marketing agency recognized for its
             immersive VR solutions and comprehensive service offerings, driving
             business growth for our clients worldwide.
           </p>
         </div>
       </div>
-      <div className="p-16 w-[75%] mx-auto bg-white">
-        <h2 className="text-[var(--primary)] text-xl underline">About Us</h2>
-        <p className="my-4 text-3xl">
+      <div className="p-8 w-[75%] mx-auto bg-white">
+        <h2 className="text-[var(--primary)] text-3xl underline">About Us</h2>
+        <p className="my-4">
           <b className="text-[var(--primary)]">Virtual Reality Marketing Agency</b> is
           a pioneering digital marketing agency that integrates immersive
           Virtual Reality (VR) experiences with traditional marketing methods.
@@ -36,18 +36,18 @@ const AboutUsPage = ({ coreValues }) => {
           <b className="text-[var(--primary)]">professional services</b>.
         </p>
       </div>
-      <div className="p-4 w-[75%] mx-auto">
-        <h3 className="text-4xl mb-6">Core Values</h3>
-        <ul className="flex flex-wrap justify-evenly">
+      <div className="py-4 px-[15%] mx-auto">
+        <h3 className="text-3xl mb-6 text-[var(--primary)]">Core Values</h3>
+        <ul className="flex flex-col md:flex-row flex-wrap justify-evenly">
           {coreValues.map((coreValue, index) => (
             <li
               key={index}
-              className="w-[40%] border rounded-4xl p-12 bg-white mb-[5%]"
+              className="md:w-[40%] border rounded-4xl p-12 bg-white mb-[5%]"
             >
-              <h4 className="text-3xl py-2 text-[var(--primary)]">
+              <h4 className="py-2 text-2xl text-[var(--primary)]">
                 {coreValue.value}
               </h4>
-              <p className="text-xl py-2">{coreValue.description}</p>
+              <p className="py-2">{coreValue.description}</p>
             </li>
           ))}
         </ul>
